57f106c33c515dcc7fedac0571fd8d318008caf9
Elhoussine200 <idmansour.elhoussine2020@gmail.com>
2024-06-07 11:49:10 +0100

    ToDoListe2

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const inputbox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+let data=[]
+
+loadDataFromLocalStorage();
+function loadDataFromLocalStorage(){
+  if(localStorage.getItem('data')){
+  let text=localStorage.getItem('data')
+  data=JSON.parse(text)
+   renderData();
+  }
+}
 
 function addTask() {
     if (inputbox.value === '') {
@@ -7,35 +17,62 @@ function addTask() {
     }
 
     else {
-        let li = document.createElement("li");
-        li.innerHTML = inputbox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+      //add new item object to data
+      let newItem={
+        name:inputbox.value
+            }
+      data.push(newItem)
 
-    }
     inputbox.value = "";
     saveData();
 }
+
+selectedItems=[]
 listContainer.addEventListener("click", function (e){
+    console.log(e.target.classList)
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
+        
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
     } ,false);
-    function saveData(){
-        localStorage.setItem("data",listContainer.innerHTML);
+  
+function saveData(){
+        //to save data in localstorage
+      localStorage.setItem("data",JSON.stringify(data));
+      renderData();
     }
-    function showTask(){
-        listContainer.innerHTML=localStorage.getItem("data");
-    }
-    showTask();
+  }
+
+function renderData(){
+        listContainer.innerHTML=""
+        // to render every element in the data into <li>
+        let i=1
+      for (let element of data){
+        let li = document.createElement("li");
+        let checkbox=document.createElement("input")
+        checkbox.type="checkbox"
+        checkbox.id="cbox"+i
+        checkbox.classList.add("mr-1")
+        let label = document.createElement("label")
+        label.htmlFor="cbox"+i
+        label.innerHTML=element.name
+
+         // Append the checkbox and label to the li element
+        li.appendChild(checkbox);
+        li.appendChild(label);
+    
+        // Append the li element to the listContainer
+        listContainer.appendChild(li);
+        i++;
+
+      }
+
+}
 
 
-
-
+  
