40755b1c10d30225e2e63ee23646e76019c345f1
Elhoussine200 <idmansour.elhoussine2020@gmail.com>
2024-06-09 21:52:47 +0100

    Add Single Delete

--- script.js
+++ script.js
@@ -1,78 +1,110 @@
 const inputbox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-let data=[]
+let data = []
 
 loadDataFromLocalStorage();
-function loadDataFromLocalStorage(){
-  if(localStorage.getItem('data')){
-  let text=localStorage.getItem('data')
-  data=JSON.parse(text)
-   renderData();
+function loadDataFromLocalStorage() {
+  if (localStorage.getItem('data')) {
+    let text = localStorage.getItem('data')
+    data = JSON.parse(text)
+    renderData();
   }
 }
 
 function addTask() {
-    if (inputbox.value === '') {
-        alert("You must write something!");
-    }
+  if (inputbox.value === '') {
+    alert("You must write something!");
+  }
 
-    else {
-      //add new item object to data
-      let newItem={
-        name:inputbox.value
-            }
-      data.push(newItem)
+  else {
+    //add new item object to data
+    let newItem = {
+      id: getUniqueID(),
+      name: inputbox.value
+    }
+    data.push(newItem)
 
     inputbox.value = "";
     saveData();
-}
-
-selectedItems=[]
-listContainer.addEventListener("click", function (e){
-    console.log(e.target.classList)
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
-        
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-    } ,false);
-  
-function saveData(){
-        //to save data in localstorage
-      localStorage.setItem("data",JSON.stringify(data));
-      renderData();
-    }
   }
 
-function renderData(){
-        listContainer.innerHTML=""
-        // to render every element in the data into <li>
-        let i=1
-      for (let element of data){
-        let li = document.createElement("li");
-        let checkbox=document.createElement("input")
-        checkbox.type="checkbox"
-        checkbox.id="cbox"+i
-        checkbox.classList.add("mr-1")
-        let label = document.createElement("label")
-        label.htmlFor="cbox"+i
-        label.innerHTML=element.name
+}
 
-         // Append the checkbox and label to the li element
-        li.appendChild(checkbox);
-        li.appendChild(label);
-    
-        // Append the li element to the listContainer
-        listContainer.appendChild(li);
-        i++;
+function getUniqueID() {
 
+  let usedIds = []
+  for (let i of data) {
+    usedIds.push(i.id)
+  }
+  usedIds.sort()
+  //get putuntiel ID
+  let tmpID = data.length + 1
+  if (usedIds.includes(tmpID)) {
+    for (let currentNumber = 0; currentNumber < usedIds.length; currentNumber++) {
+      if (usedIds[currentNumber + 1] - usedIds[currentNumber] > 1) {
+        tmpID = usedIds[currentNumber] + 1
       }
+    }
 
+  }
+  //case 1 tmpID not used 
+
+  return tmpID;
 }
 
 
-  
+selectedItems = []
+
+
+function saveData() {
+  //to save data in localstorage
+  localStorage.setItem("data", JSON.stringify(data));
+  renderData();
+}
+
+
+
+
+
+function renderData() {
+  listContainer.innerHTML = ""
+  // to render every element in the data into <li>
+  let i = 1
+  for (let element of data) {
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input")
+    let label = document.createElement("label")
+    let deleteBtn = document.createElement("span")
+
+    checkbox.type = "checkbox"
+    checkbox.id = "cbox" + i
+    checkbox.classList.add("mr-1")
+
+    label.htmlFor = "cbox" + i
+    label.innerHTML = element.name
+
+    deleteBtn.classList.add("deleteBtn")
+    deleteBtn.innerHTML = "❌"
+    deleteBtn.dataset.itemId = element.id
+
+
+
+    // Append the checkbox and label to the li element
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(deleteBtn);
+
+    // Append the li element to the listContainer
+    listContainer.appendChild(li);
+    i++;
+
+  }
+}
+
+
+document.getElementById("list-container").addEventListener("click", function (e) {
+  let selectedId = e.target.dataset.itemId;
+  data = data.filter(item => item.id != selectedId)
+  saveData();
+})
+
